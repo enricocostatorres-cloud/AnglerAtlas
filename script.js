a76f6a0c113f947ffc4c6c23a5d9dbfa4cae5f30
enricocostatorres-cloud <enrico.costatorres@ucdconnect.ie>
2026-02-11 16:37:48 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code modification
-
-// Other parts of the script...
-
-// Change made on 2026-02-11 16:16:59 UTC
-window.location.href = "dashboard.html";
-// Other parts of the script...
